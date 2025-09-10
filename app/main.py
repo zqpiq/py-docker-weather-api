a93f61ac61be6e3b8a4214e953842d28ad5f8000
Tamerlan <egtamerlan@gmail.com>
2025-09-10 19:26:05 +0200

    sol

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+from typing import Any
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_weather() -> dict[str, Any]:
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ValueError("API_KEY does not exist")
+    city = "Paris"
+    url = "https://api.weatherapi.com/v1/current.json"
+    params = {"key": api_key, "q": city}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    data = response.json()
+    return data
 
 
 if __name__ == "__main__":
