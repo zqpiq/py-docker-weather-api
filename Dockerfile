67a92bca9cda6cc237a79a0d2a54588bd98f35fd
Tamerlan <egtamerlan@gmail.com>
2025-09-11 12:15:18 +0200

    sol

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["python", "app/main.py"]
+CMD ["python", "manage.py", "runserver", "8000"]
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any
 
 import requests
@@ -7,20 +8,16 @@ load_dotenv()
 
 
 def get_weather() -> dict[str, Any]:
-    url = "https://wttr.in/Paris?format=j1"
-    response = requests.get(url)
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ValueError("API_KEY does not exist")
+    city = "Paris"
+    url = "http://api.weatherapi.com/v1/current.json"
+    params = {"key": api_key, "q": city}
+    response = requests.get(url, params=params)
     response.raise_for_status()
     data = response.json()
-    current = data["current_condition"][0]
-    temp_c = current["temp_C"]
-    speed_wind = current["windspeedKmph"]
-    visibility = current["visibility"]
-    return {
-        "city": "Paris",
-        "celsius": temp_c,
-        "wind speed": speed_wind,
-        "visibility": visibility
-    }
+    return data
 
 
 if __name__ == "__main__":
